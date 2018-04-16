5506b5f56165a9df5da6c36da42886abbb048fc1
MOHAMMAD RASIM <mohammad.rasim96@gmail.com>
2018-04-16 22:00:59 +0300

    Add dual subtitle support

--- service.py
+++ service.py
@@ -9,6 +9,9 @@ import xbmcaddon
 import xbmcgui,xbmcplugin
 import xbmcvfs
 import uuid
+import json
+import pysubs2
+import chardet
 
 __addon__ = xbmcaddon.Addon()
 __author__     = __addon__.getAddonInfo('author')
@@ -44,6 +47,7 @@ def Search( item ):
 				     not os.path.splitext(x['SubFileName'])[0] == os.path.splitext(os.path.basename(urllib.unquote(item['file_original_path'])))[0],
 				     not normalizeString(xbmc.getInfoLabel("VideoPlayer.OriginalTitle")).lower() in x['SubFileName'].replace('.',' ').lower(),
 				     not x['LanguageName'] == PreferredSub])
+    listitems=[]
     for item_data in search_data:
       ## hack to work around issue where Brazilian is not found as language in XBMC
       if item_data["LanguageName"] == "Brazilian":
@@ -67,9 +71,24 @@ def Search( item ):
                                                                           item_data["SubFileName"],
                                                                           item_data["SubFormat"]
                                                                           )
-
+        listitems.append(listitem)
+        if(__addon__.getSetting('dualsub_enable') != 'Yes'):
+          xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=listitem,isFolder=False)
+    if(__addon__.getSetting('dualsub_enable') == 'Yes'):
+      dialog = xbmcgui.Dialog()
+      ret = dialog.multiselect("Choose a subtitle", [i for i in listitems],useDetails=True)
+      if ret and len(ret) > 0:
+        subs=[]
+        for sub in ret:
+          subs.append({'ID':search_data[sub]['IDSubtitleFile'],
+            'link':search_data[sub]['ZipDownloadLink'],
+            'filename':search_data[sub]['SubFileName'],
+            'format':search_data[sub]['SubFormat']})
+        payload=json.dumps(subs[:2])
+        payload=urllib.quote(payload)
+        listitem = xbmcgui.ListItem(label2='Download and Combine subtitle')
+        url = "plugin://%s/?action=download&payload=%s"% (__scriptid__,payload)
         xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=listitem,isFolder=False)
-
 
 def Download(id,url,format,stack=False):
   subtitle_list = []
@@ -142,6 +161,64 @@ def get_params(string=""):
 
   return param
 
+def charset_detect(filename):
+    with open(filename,'rb') as fi:
+        rawdata = fi.read()
+    encoding = chardet.detect(rawdata)['encoding']
+    if encoding.lower() == 'gb2312':  # Decoding may fail using GB2312
+        encoding = 'gbk'
+    return encoding
+
+
+def merge(file):
+    subs=[]
+    for sub in file:
+      subs.append(pysubs2.load(sub, encoding=charset_detect(sub)))
+    ass = os.path.join(__temp__, "%s.ass" %(str(uuid.uuid4())))
+    top_style = pysubs2.SSAStyle()
+    bottom_style=subs[0].styles["Default"].copy()
+    top_style.alignment=8
+    top_style.fontsize=__addon__.getSetting('top_fontsize')
+    top_style.bold = __addon__.getSetting('top_bold')
+    top_style.bold = __addon__.getSetting('top_font')
+
+    if (__addon__.getSetting('top_color') == 'Yellow'):
+      top_style.primarycolor=pysubs2.Color(255, 255, 0, 0)
+    elif (__addon__.getSetting('top_color') == 'White'):
+      top_style.primarycolor=pysubs2.Color(255, 255, 255, 0)
+    #    top_style.secondarycolor=pysubs2.Color(255,255,255,0)
+    if (__addon__.getSetting('top_background') == 'Yes'):
+      top_style.backcolor=pysubs2.Color(0,0,0,128)
+      top_style.outlinecolor=pysubs2.Color(0,0,0,128)
+      top_style.shadow=0
+      top_style.outline=0
+      top_style.borderstyle=4
+    bottom_style.alignment = 2
+    bottom_style.fontsize=__addon__.getSetting('bottom_fontsize')
+    bottom_style.bold = __addon__.getSetting('bottom_bold')
+    bottom_style.bold = __addon__.getSetting('bottom_font')
+    if (__addon__.getSetting('bottom_color') == 'Yellow'):
+      bottom_style.primarycolor=pysubs2.Color(255, 255, 0, 0)
+    elif (__addon__.getSetting('bottom_color') == 'White'):
+      bottom_style.primarycolor=pysubs2.Color(255, 255, 255, 0)
+    if (__addon__.getSetting('bottom_background') == 'Yes'):
+      bottom_style.backcolor=pysubs2.Color(0,0,0,128)
+      bottom_style.outlinecolor=pysubs2.Color(0,0,0,128)
+      bottom_style.shadow=0
+      bottom_style.outline=0
+      bottom_style.borderstyle=4
+
+    subs[0].styles['top-style'] = top_style
+    subs[0].styles['bottom-style'] = bottom_style
+    for line in subs[0]:
+      line.style= u'bottom-style'
+    if len(subs) ==2:
+      for line in subs[1]:
+        line.style = u'top-style'
+        subs[0].append(line)
+    subs[0].save(ass,format_='ass')
+    return ass
+
 params = get_params()
 
 if params['action'] == 'search' or params['action'] == 'manualsearch':
@@ -210,10 +287,19 @@ if params['action'] == 'search' or params['action'] == 'manualsearch':
   Search(item)
 
 elif params['action'] == 'download':
-  subs = Download(params["ID"], params["link"],params["format"])
-  for sub in subs:
-    listitem = xbmcgui.ListItem(label=sub)
-    xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=sub,listitem=listitem,isFolder=False)
+  if(__addon__.getSetting('dualsub_enable') == 'Yes'):
+    payload=json.loads(urllib.unquote(params['payload']))
+    subs=[]
+    for sub in payload:
+      subs.append(Download(sub["ID"], sub["link"],sub["format"])[0])
+    finalfile = merge(subs)
+    listitem = xbmcgui.ListItem(label=finalfile)
+    xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=finalfile,listitem=listitem,isFolder=False)
+  else:
+    subs = Download(params["ID"], params["link"],params["format"])
+    for sub in subs:
+      listitem = xbmcgui.ListItem(label=sub)
+      xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=sub,listitem=listitem,isFolder=False)
 
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
